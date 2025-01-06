01c835ec38a3b4030586b63357b967e3278a0b80
Ibragim Baharchiev <gumakov111@gmail.com>
2025-01-06 03:24:46 +0300

    add new two methods: add and size

--- src/main/java/My_Map.java
+++ src/main/java/My_Map.java
@@ -1,2 +1,24 @@
-public class My_Map {
+public class My_Map<K, V> {
+    private static Object[] keys;
+    private static Object[] values;
+    private static int size;
+
+    public My_Map() {
+        keys = new Object[10];
+        values = new Object[10];
+        size = 0;
+    }
+
+    public void add(K key, V value) {
+        keys[size] = key;
+        values[size] = value;
+        ++size;
+
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    
 }
