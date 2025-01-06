f6e5142015af055069078246861da0c4ef307830
Ibragim Baharchiev <gumakov111@gmail.com>
2025-01-06 04:03:26 +0300

    add new method increase_Arrays

--- src/main/java/My_Map.java
+++ src/main/java/My_Map.java
@@ -1,11 +1,16 @@
-public class My_Map<K, V> {
-    private static Object[] keys;
-    private static Object[] values;
-    private static int size;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class My_Map<K, V> implements Iterable {
+    private Object[] keys;
+    private Object[] values;
+    private int size;
+    private int length = 10;
 
     public My_Map() {
-        keys = new Object[10];
-        values = new Object[10];
+        keys = new Object[length];
+        values = new Object[length];
         size = 0;
     }
 
@@ -13,12 +18,48 @@ public class My_Map<K, V> {
         keys[size] = key;
         values[size] = value;
         ++size;
+        if (size == length) {
+            increase_Arrays();
+        }
 
     }
 
-    public static int size() {
+    public int size() {
         return size;
     }
 
-    
+    private void increase_Arrays() {
+        if (size == length) {
+            length = length * 2;
+            //know a new length of new massive
+            Object[] keys = new Object[length];
+            Object[] values = new Object[length];
+            for (int i = 0; i < size; i++) {
+                keys[i] = this.keys[i];
+                values[i] = this.values[i];
+            }
+            this.keys = keys;
+            this.values = values;
+            //create a new two massive
+            // send it all the date from old massive
+
+
+        }
+    }
+
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer action) {
+        Iterable.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator spliterator() {
+        return Iterable.super.spliterator();
+    }
 }
